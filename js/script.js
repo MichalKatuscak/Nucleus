4b00f00af70716922ceca20cb92274fc68e1d5e6
Michal Katuščák <katuscak@nexgen.cz>
2015-07-23 15:34:11 +0200

    Rozšíření základního skriptu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
+    // Spustí se při načtení DOMu (struktura bez externích souborů - obrázků)
+});
 
+$(window).load(function() {
+    // Spustí se po kompletním načetení stránky, důležitě především pro obrázky
 });
 
 
